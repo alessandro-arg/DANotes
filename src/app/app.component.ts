29db496c244d70b7a90a0961b5ce651d7b29609b
Alessandro Argenziano <1603alessandro@gmail.com>
2025-04-12 13:50:20 +0200

    firebase is correctly linked to the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { NoteListComponent } from './note-list/note-list.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { AddNoteDialogComponent } from './add-note-dialog/add-note-dialog.component';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NoteListComponent, FooterComponent, HeaderComponent, AddNoteDialogComponent],
+  imports: [
+    CommonModule,
+    NoteListComponent,
+    FooterComponent,
+    HeaderComponent,
+    AddNoteDialogComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'DAKeep';
--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,55 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Note } from '../interfaces/note.interface';
+import {
+  Firestore,
+  collectionData,
+  collection,
+  doc,
+  onSnapshot,
+} from '@angular/fire/firestore';
+import { elementAt } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NoteListService {
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
 
-  constructor() { }
+  firestore: Firestore = inject(Firestore);
+  items$ = collectionData(this.getNotesRef());
+
+  items;
+  unsubList;
+
+  constructor() {
+    this.unsubList = onSnapshot(this.getNotesRef(), (list) => {
+      list.forEach((element) => {
+        console.log(element);
+      });
+    });
+
+    this.items = this.items$.subscribe((list) => {
+      list.forEach((element) => {
+        console.log(element);
+      });
+    });
+  }
+
+  ngOnDestroy() {
+    this.unsubList();
+    this.items.unsubscribe();
+  }
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getSingleDocRef(colId: string, docId: string) {
+    return doc(collection(this.firestore, colId), docId);
+  }
 }
